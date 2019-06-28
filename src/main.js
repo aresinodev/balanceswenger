8947e58d273a95d0b917ef374584f9837b9a4cb2
Aarón RJ <aaron@quantumstudio.es>
2019-06-28 10:58:34 +0200

    - Instalado vue-router. - Creado el fichero de rutas y añadido al main.js.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+import routes from './routes/routes';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.config.devtools = true;
+
+Vue.use(VueRouter);
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  routes
+})
+  .$mount('#app');
