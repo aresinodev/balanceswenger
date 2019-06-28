c2fd34cade9331a6140ba68bb426dd3c67c6b465
Aarón RJ <aaron@quantumstudio.es>
2019-06-28 12:25:56 +0200

    - Generado el componente login y añadido al app.vue para probar.

---
+++ src/components/login/Login.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+
+  <section class="src-components-login">
+    <h1>src-components-login Component</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+  export default  {
+    name: 'src-components-login',
+    props: [],
+    mounted() {
+
+    },
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+</script>
+
+<style scoped lang="scss">
+  @import 'login.scss';
+</style>
